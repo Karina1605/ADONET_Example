3897c6d868a2b613c894141d51a85017ec7ca32a
Karina1605 <akchurinakarina1605@yandex.ru>
2021-09-08 13:11:43 +0300

    add tables

---
+++ DataBaseModels/DataBaseTables/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace DataBaseModels.DataBaseTables
+{
+    class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public float Cost { get; set; }
+        [Required]
+        public int Count { get; set; }
+        public int CategoryId { get; set; }
+        public int ColorId { get; set; }
+        public string PhotoPath { get; set; }
+    }
+}
