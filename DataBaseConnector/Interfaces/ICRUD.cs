b9060cbf57b08ba0e831981137103dabc16d1b8e
Karina1605 <akchurinakarina1605@yandex.ru>
2021-09-08 14:24:18 +0300

    interfaces added

---
+++ DataBaseConnector/Interfaces/ICRUD.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataBaseConnector.Interfaces
+{
+    public interface ICRUD<T>
+    {
+        public T GetById(int Id);
+        public IEnumerable<T> GetAll();
+        public void Insert(T item);
+        public void Update(T item);
+        public void delete(int Id);
+    }
+}
